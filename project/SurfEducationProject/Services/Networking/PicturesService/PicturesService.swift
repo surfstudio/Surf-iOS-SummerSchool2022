d072228195e440b669efd0ce0f1abcdff378937d
Ilya Knyazkov <fivecoil@gmail.com>
2022-08-08 13:34:16 +0300

    Add response with array

--- project/SurfEducationProject/Services/Networking/PicturesService/PicturesService.swift
+++ project/SurfEducationProject/Services/Networking/PicturesService/PicturesService.swift
@@ -9,13 +9,13 @@ import Foundation
 
 struct PicturesService {
 
-    let dataTask = BaseNetworkTask<EmptyModel, PictureResponseModel>(
+    let dataTask = BaseNetworkTask<EmptyModel, [PictureResponseModel]>(
         inNeedInjectToken: true,
         method: .get,
         path: "picture/"
     )
 
-    func loadPictures(_ onResponseWasReceived: @escaping (_ result: Result<PictureResponseModel, Error>) -> Void) {
+    func loadPictures(_ onResponseWasReceived: @escaping (_ result: Result<[PictureResponseModel], Error>) -> Void) {
         dataTask.performRequest(onResponseWasReceived)
     }
 
