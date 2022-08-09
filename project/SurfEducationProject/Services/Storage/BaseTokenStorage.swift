21494f266e53e0d7a16571487e1c11ad04d5bd1d
Ilya Knyazkov <fivecoil@gmail.com>
2022-08-09 13:42:53 +0300

    Implement `BaseTokenStorage`

--- project/SurfEducationProject/Services/Storage/BaseTokenStorage.swift
+++ project/SurfEducationProject/Services/Storage/BaseTokenStorage.swift
@@ -9,12 +9,115 @@ import Foundation
 
 struct BaseTokenStorage: TokenStorage {
 
-    func getToken() throws -> TokenContainer {
-        TokenContainer(
-            token: "13dafde943a580620627b934bb12a39ed589514ef42b479e82a56b15451460fc"
-        )
+    // MARK: - Nested Types
+
+    private enum Constants {
+        static let applicationNameInKeyChain = "com.surf.education.project"
+        static let tokenKey = "token"
+        static let tokenDatekey = "tokenDate"
     }
 
-    func set(newToken: TokenContainer) throws { }
+    // MARK: - Private Properties
+
+    private var unprotectedStorage: UserDefaults {
+        UserDefaults.standard
+    }
+
+    // MARK: - TokenStorage
+
+    func getToken() throws -> TokenContainer {
+
+        let queryDictionaryForSavingToken: [CFString: AnyObject] = [
+            kSecAttrService: Constants.applicationNameInKeyChain as AnyObject,
+            kSecAttrAccount: Constants.tokenKey as AnyObject,
+            kSecClass: kSecClassGenericPassword,
+            kSecMatchLimit: kSecMatchLimitOne,
+            kSecReturnData: kCFBooleanTrue
+        ]
+
+        var tokenInResult: AnyObject?
+        let status = SecItemCopyMatching(queryDictionaryForSavingToken as CFDictionary, &tokenInResult)
+
+        try throwErrorFromStatusIfNeeded(status)
+
+        guard let data = tokenInResult as? Data else {
+            throw Error.tokenWasNotFoundInKeyChainOrCantRepresentAsData
+        }
+
+        let retrivingToken = try JSONDecoder().decode(String.self, from: data)
+        let tokenSavingDate = try getSavingTokenDate()
+
+        return TokenContainer(token: retrivingToken, receivingDate: tokenSavingDate)
+    }
+
+    func set(newToken: TokenContainer) throws {
+        try removeTokenFromContainer()
+
+        let tokenInData = try JSONEncoder().encode(newToken.token)
+        let queryDictionaryForSavingToken: [CFString: AnyObject] = [
+            kSecAttrService: Constants.applicationNameInKeyChain as AnyObject,
+            kSecAttrAccount: Constants.tokenKey as AnyObject,
+            kSecClass: kSecClassGenericPassword,
+            kSecValueData: tokenInData as AnyObject
+        ]
+
+        let status = SecItemAdd(queryDictionaryForSavingToken as CFDictionary, nil)
+
+        try throwErrorFromStatusIfNeeded(status)
+
+        saveTokenSavingDate(.now)
+    }
+
+    func removeTokenFromContainer() throws {
+        let queryDictionaryForDeleteToken: [CFString: AnyObject] = [
+            kSecAttrService: Constants.applicationNameInKeyChain as AnyObject,
+            kSecAttrAccount: Constants.tokenKey as AnyObject,
+            kSecClass: kSecClassGenericPassword
+        ]
+
+        let status = SecItemDelete(queryDictionaryForDeleteToken as CFDictionary)
+
+        try throwErrorFromStatusIfNeeded(status)
+
+        removeTokenSavingDate()
+    }
+
+}
+
+private extension BaseTokenStorage {
+
+    enum Error: Swift.Error {
+        case unknownError(status: OSStatus)
+        case keyIsAlreadyInKeyChain
+        case tokenWasNotFoundInKeyChainOrCantRepresentAsData
+        case tokenDateWasNotFound
+    }
+
+    func getSavingTokenDate() throws -> Date {
+        guard let savingDate = unprotectedStorage.value(forKey: Constants.tokenDatekey) as? Date else {
+            throw Error.tokenDateWasNotFound
+        }
+
+        return savingDate
+    }
+
+    func saveTokenSavingDate(_ newDate: Date) {
+        unprotectedStorage.set(newDate, forKey: Constants.tokenDatekey)
+    }
+
+    func removeTokenSavingDate() {
+        unprotectedStorage.set(nil, forKey: Constants.tokenDatekey)
+    }
+
+    func throwErrorFromStatusIfNeeded(_ status: OSStatus) throws {
+        guard status == errSecSuccess || status == -25300 else {
+            throw Error.unknownError(status: status)
+        }
+
+
+        guard status != -25299 else {
+            throw Error.keyIsAlreadyInKeyChain
+        }
+    }
 
 }
